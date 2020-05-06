a0ab3a40ffa76d10dd272f5ed84941914a9d7cbc
yeldev <mycops18@gmail.com>
2020-05-06 09:30:06 +0300

    add workbox (#closes 29)

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Workbox = require("workbox-webpack-plugin");
 
 module.exports = merge(commonConfig, {
   mode: "production",
@@ -34,6 +35,15 @@ module.exports = merge(commonConfig, {
     new MiniCssExtractPlugin({
       filename: "./static/css/[name].[contentHash].css"
     }),
+    new Workbox.GenerateSW({
+      swDest: "sw.js",
+      skipWaiting: true,
+      clientsClaim: true,
+      runtimeCaching: [{
+        urlPattern: new RegExp('https://yeldev.github.io/homepage/'),
+        handler: 'StaleWhileRevalidate'
+      }]
+    })
   ],
   module: {
     rules: [
